210eb2bb9c2aa8623817082fe8db656814a657a0
Zhiyang Chen <1260678143@qq.com>
2025-04-14 21:12:57 +0800

    revise

--- lesson_02_Python入门/code/08.布尔值和空值.py
+++ lesson_02_Python入门/code/08.布尔值和空值.py
@@ -4,10 +4,10 @@
 # True表示真 False表示假
 a = True
 a = False
-# print('a =',a)
+print('a =',a)
 
 # 布尔值实际上也属于整型，True就相当于1，False就相当于0
-# print(1 + False)
+print(1 + False)
 
 # None（空值）
 # None专门用来表示不存在
